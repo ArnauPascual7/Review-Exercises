a9271a2f9f305fcdc8d44c8b14e31f9ae9443b06
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-01-08 12:44:44 +0100

    Exercise 1 Done #1

--- Review-Exercises/MyLibrary/Math.cs
+++ Review-Exercises/MyLibrary/Math.cs
@@ -1,7 +1,20 @@
 ﻿namespace MyLibrary
 {
-    public class Math
+    public static class Math
     {
+        public static string SecondsToTime(int seconds)
+        {
+            int minutes = 0;
+            int hours = 0;
 
+            if (seconds < 0) throw new Exception("ERROR! S'ESPERA UN NOMBRE POSITIU");
+
+            minutes = seconds / 60;
+            hours = minutes / 60;
+            seconds = seconds % 60;
+            minutes = minutes % 60;
+
+            return $"{hours} hores {minutes} minuts {seconds} segons";
+        }
     }
 }
