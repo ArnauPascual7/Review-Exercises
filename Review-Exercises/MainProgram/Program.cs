ca7f707d66746e1d3a422ccb845cbfb751c7cf3b
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-01-19 18:22:47 +0100

    Exercise 5 #9

--- Review-Exercises/MainProgram/Program.cs
+++ Review-Exercises/MainProgram/Program.cs
@@ -5,7 +5,28 @@ namespace MainProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string Msg = "Introdueix una temperatiura en Kelvin:";
+            const string MsgResult = "Kelvin: {0} - Fahrenheit: {1}";
+            const string ErrReadingNum = "ERROR! El valor no es un número";
+
+            float kelvin = 0;
+            bool flag = true;
+
+            do
+            {
+                Console.WriteLine(Msg);
+
+                flag = !float.TryParse(Console.ReadLine(), out kelvin);
+
+                if (flag)
+                {
+                    Console.WriteLine(ErrReadingNum);
+                }
+            } while (flag);
+
+            float fahrenheit = MyLibrary.Math.KelvinToFahrenheit(kelvin);
+
+            Console.WriteLine(MsgResult, kelvin, fahrenheit);
         }
     }
 }
