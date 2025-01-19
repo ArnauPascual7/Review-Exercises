a4918f1a077019f354d9b8978984540189f2beb4
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-01-19 20:13:12 +0100

    Exercise 7 Done #13

--- Review-Exercises/MyLibrary/Math.cs
+++ Review-Exercises/MyLibrary/Math.cs
@@ -2,6 +2,44 @@
 {
     public class Math
     {
+        public static string DecimalToHexadecimal(int _decimal)
+        {
+            int res = 0;
+            string hexadecimal = "";
 
+            if (_decimal >= 16)
+            {
+                while (_decimal >= 16)
+                {
+                    res = _decimal % 16;
+
+                    hexadecimal = CheckHexadecimalLetter(res) + hexadecimal;
+
+                    _decimal = _decimal / 16;
+                }
+            }
+            hexadecimal = CheckHexadecimalLetter(_decimal) + hexadecimal;
+            return hexadecimal;
+        }
+        public static string CheckHexadecimalLetter(int num)
+        {
+            switch (num)
+            {
+                case 10:
+                    return "A";
+                case 11:
+                    return "B";
+                case 12:
+                    return "C";
+                case 13:
+                    return "D";
+                case 14:
+                    return "E";
+                case 15:
+                    return "F";
+                default:
+                    return $"{num}";
+            }
+        }
     }
 }
