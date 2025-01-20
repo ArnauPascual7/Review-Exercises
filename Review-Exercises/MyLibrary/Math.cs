d8f790a6d2f498e5b3cf0db0d0815def083de479
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-01-20 19:50:34 +0100

    Exercise 12 Done #23

--- Review-Exercises/MyLibrary/Math.cs
+++ Review-Exercises/MyLibrary/Math.cs
@@ -2,6 +2,13 @@
 {
     public class Math
     {
-
+        public static bool CheckIfArrayOrdered(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1]) return false;
+            }
+            return true;
+        }
     }
 }
